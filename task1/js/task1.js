ada877b7d89ce068398ac7368f6f15107d9b35db
wanxxxx <37324189+wanxxxx@users.noreply.github.com>
2018-04-09 20:25:13 +0800

    2end

---
+++ task1/js/task1.js
@@ -0,0 +1,45 @@
+function tiao() {
+	window.open('../task2.html');
+}
+
+function arra(m) { //0-m的随机排列数组
+	var arr = new Array(m);
+	arr[0] = 0;
+	for (var i = 1; i < m; i++) {
+		var rnd = Math.floor(Math.random() * (i + 1));
+		arr[i] = arr[rnd];
+		arr[rnd] = i;
+	}
+	return arr;
+}
+var arr2 = document.getElementsByName('unknown'); //将name为unknown的元素组成一个集合
+
+function checkField(val) {
+	var arr1 = arra(val);
+	if (val >= 6 && val <= 18) {
+		civilian.value = val - killer.value;
+	} else {
+		alert("请输入正确数字")
+	}
+	for (i = 0; i < val; i++) { //全赋予平民
+		arr2[arr1[i]].value = "1";
+	}
+	if (val >= 6 && val <= 8) {
+		killer.value = 1;
+	} else if (val >= 9 && val <= 11) {
+		killer.value = 2;
+	} else if (val >= 12 && val <= 15) {
+		killer.value = 3;
+	} else if (val >= 16 && val <= 18) {
+		killer.value = 4;
+	} else {
+		alert("请输入正确数字")
+	}
+	for (i = 0; i < val; i++) { //全赋予平民
+		arr2[arr1[i]].value = "";
+	}
+	for (i = 0; i < killer.value; i++) { //随机赋予杀手
+		arr2[arr1[i]].value = "杀手";
+	}
+
+}
---
+++ task2/js/555.js
@@ -0,0 +1,11 @@
+function tiao() {
+	window.open('../task2.html');
+}
+
+var killer = document.getElementById("killer");
+var civilian = document.getElementById("civilian");
+var player = document.getElementById("player");
+
+function a(val) {
+	
+}
