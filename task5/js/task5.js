4d29ce0705df4f0c1bad773fb23126b781b89f7d
wanxxxx <37324189+wanxxxx@users.noreply.github.com>
2018-08-30 16:05:07 +0800

    1

--- task5/js/task5.js
+++ task5/js/task5.js
@@ -1,8 +1,22 @@
- $(function() {
-     $("#btnShow").bind("click", function() {
-         var $this = $(this);
-         $("ul").html("<img src='Images/Loading.gif' alt=''/>").load("http://www.imooc.com/data/fruit_part.html", function() {
-             $this.attr("disabled", "true");
-         });
-     })
- });
+$("form").on("submit", function() {
+    var name = $('#name').text();
+    var pwd = $('#pwd').text();
+    $.ajax({
+        type: 'POST',
+        url: '/carrots-admin-ajax/a/login',
+        data: $('#signin').serialize(),
+        dataType: 'json',
+        success: function(data) {
+            if (data.code === 0) {
+                window.location.href = "http://dev.admin.carrots.ptteng.com/";
+            } else {
+                if (name === '' || pwd === '') {
+                    $('#msg').text('请输入用户名和密码');
+                } else {
+                    $('#msg').text(data.message);
+                }
+            }
+        }
+    })
+    return false; //阻止表单默认提交行为
+})
